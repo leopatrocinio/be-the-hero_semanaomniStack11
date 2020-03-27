dc5c0df9643e799fb1e343dc61a04fe6d6641d27
leopatrocinio <leonardo20.0@hotmail.com>
2020-03-27 16:46:43 -0300

    validação e testes da aplicação

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require("cors");
 const routes = require('./routes');
+const { errors } = require('celebrate');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
+
+module.exports = app;
 
 /**
  *  Rota / Recurso
@@ -41,5 +45,3 @@ app.use(routes);
    */
 
 
-
-app.listen(3333);
